a05cb3d6585b65aa1f641e98ec79dab7b8a3d3d2
akif bade <acshona143@gmail.com>
2025-09-09 15:52:26 +0000

    Bhai firebase kyu create kar rahe ho wo to already hai

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const G = (id) => D.getElementById(id);
 // --- Firebase Configuration ---
 // IMPORTANT: Replace with your actual Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    "apiKey": "mock-api-key",
+    "authDomain": "qgo-cargo-survey-app-static.firebaseapp.com",
+    "projectId": "qgo-cargo-survey-app-static",
+    "storageBucket": "qgo-cargo-survey-app-static.appspot.com",
+    "messagingSenderId": "1082571212151",
+    "appId": "1:1082571212151:web:d9333a5585b4f3b890a202"
 };
 
 let db; // To hold the Firestore instance
